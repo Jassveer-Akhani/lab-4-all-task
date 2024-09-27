a95f536a765826ffd2d0dc659ec385322e084f47
Jassveer-Akhani <155067689+Jassveer-Akhani@users.noreply.github.com>
2024-09-27 15:11:15 +0500

    Update Untitled2.cpp

--- Lab-5/Untitled2.cpp
+++ Lab-5/Untitled2.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
-int main() {
-    float per;
-    char gr;
-    printf("Enter your percentage: ");
-    scanf("%f", &per);
-    gr = (per >= 90) ? 'A' :
-         (per >= 80) ? 'B' :
-         (per >= 70) ? 'C' :
-        (per >= 60) ? 'D' : 'F';
-    printf("Your grade is: %c\n", gr);
-
-    return 0;
+int main (){
+	int age;
+	char c;
+	printf("Do you have citizenship of Pakistan type y for yes n for No: ");
+	scanf("%c", &c);
+	printf("Enter Your age; ");
+	scanf("%d", &age);
+	if  (c == 'y' && age >= 18){
+		printf("you are eligible for vote");
+		} 
+		else {
+			printf("you are not eligible for vote");
+		}
+		return  0;
 }
 
 
